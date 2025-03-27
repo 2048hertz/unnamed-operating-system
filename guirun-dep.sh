d1d690e070ade79ff64415edfa1b3b91e789655a
Ayaan Eusufzai <97022894+2048hertz@users.noreply.github.com>
2025-03-27 07:58:21 +0600

    Create guirun-dep.sh

---
+++ guirun-dep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Written by Ayaan Eusufzai
+# Installs dependencies for gui-run
+
+echo "[*] Installing Xorg and basic graphical tools..."
+
+xbps-install -S \
+  xorg-minimal \
+  xinit \
+  xterm \
+  openbox \
+  firefox \
+  gimp \
+  psmisc \
+  flatpak \
+  xrandr \
+  xdg-utils
+
+echo "[+] GUI dependencies installed successfully."
